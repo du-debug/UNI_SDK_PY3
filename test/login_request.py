9d3d6ddc7a4fccb1e885013e7cfb1467f1a80784
duweihua <weihua.du@zqgame.com>
2020-01-10 16:12:46 +0800

    01

--- test/login_request.py
+++ test/login_request.py
@@ -8,11 +8,15 @@ class LoginRequest(object):
     keys = ('account', 'session', 'ext', 'sign')
 
     def __init__(self, *args, **kwargs):
+        super(LoginRequest, self).__init__()
+        self._app = kwargs['app']
+        self._mysql = kwargs['mysql']
+        self._platform_info = kwargs['platform_info']
         pass
 
     def process(self, request_handler, params):
         """由此处理具体业务逻辑"""
-        print(params)
+
         return True
 
     def get_params_keys(self):
